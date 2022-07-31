39be34bd5656b1eaa34b9826a2a74eda24aa9f05
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2022-07-31 14:39:21 -0600

    idk

--- react-app/src/pages/Home.js
+++ react-app/src/pages/Home.js
@@ -28,13 +28,13 @@ function HomePage() {
       <br></br>
       
       <div className='social-container'>
-        <a href='https://twitter.com/kyledickeyy' target="_blank" rel="noreferrer" className="fa fa-brands fa-twitter fa-3x socialIcon"></a>
+        <a href='https://twitter.com/kyledickeyy' target="_blank" rel="noreferrer" className="fa fa-brands fa-twitter fa-3x socialIcon"> </a>
 
-        <a href='https://instagram.com/kyle.dickeyy' target="_blank" rel="noreferrer" className="fa fa-brands fa-instagram fa-3x socialIcon"></a>
+        <a href='https://instagram.com/kyle.dickeyy' target="_blank" rel="noreferrer" className="fa fa-brands fa-instagram fa-3x socialIcon"> </a>
 
-        <a href='https://github.com/dickeyy' target="_blank" rel="noreferrer" className="fa fa-brands fa-github fa-3x socialIcon"></a>
+        <a href='https://github.com/dickeyy' target="_blank" rel="noreferrer" className="fa fa-brands fa-github fa-3x socialIcon"> </a>
 
-        <a href='https://twitch.tv/imdickey' target="_blank" rel="noreferrer" className="fa fa-brands fa-twitch fa-3x socialIcon"></a>
+        <a href='https://twitch.tv/imdickey' target="_blank" rel="noreferrer" className="fa fa-brands fa-twitch fa-3x socialIcon"> </a>
       </div>
 
       </div>
@@ -44,9 +44,9 @@ function HomePage() {
       <div className='project-container'>
 
       <p className='proj-title'>Projects</p>
-      <p className='proj-subtitle'>Double click to expand (Not in any order)</p>
+      <p className='proj-subtitle'>Click to expand (Not in any order)</p>
 
-      <button class="accordion" onClick={() => { accClick() }}>DoxBot</button>
+      <button id='acc-1' class="accordion" onClick={() => { accClick('acc-1') }}>DoxBot</button>
       <div class="panel">
         <p>The last Discord bot you'll ever need</p>
 
@@ -55,7 +55,7 @@ function HomePage() {
         <a className='proj-link' href='https://doxbot.xyz' target="_blank" rel="noreferrer">Learn More</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>LasTag</button>
+      <button id='acc-2' class="accordion" onClick={() => { accClick('acc-2') }}>LasTag</button>
       <div class="panel">
         <p>The last gamer tag you'll ever need.</p>
 
@@ -64,7 +64,7 @@ function HomePage() {
         <a className='proj-link' href='https://lastag.xyz' target="_blank" rel="noreferrer">Learn More</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Alias</button>
+      <button id='acc-3' class="accordion" onClick={() => { accClick('acc-3') }}>Alias</button>
       <div class="panel">
         <p>The fun for all ages party game... Now modernized!</p>
 
@@ -73,7 +73,7 @@ function HomePage() {
         <a className='proj-link' href='https://aliasgame.xyz' target="_blank" rel="noreferrer">Play</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Counting.exe</button>
+      <button id='acc-4' class="accordion" onClick={() => { accClick('acc-4') }}>Counting.exe</button>
       <div class="panel">
         <p>The Twitter bot that counts once a day, that's it.</p>
 
@@ -82,7 +82,7 @@ function HomePage() {
         <a className='proj-link' href='https://twitter.com/countingdotexe' target="_blank" rel="noreferrer">Learn More</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Collatz Collab</button>
+      <button id='acc-5' class="accordion" onClick={() => { accClick('acc-5') }}>Collatz Collab</button>
       <div class="panel">
         <p>The collaborative project to solve the hardest math problem ever.</p>
 
@@ -91,7 +91,7 @@ function HomePage() {
         <a className='proj-link' href='https://collatzcollab.com' target="_blank" rel="noreferrer">Learn More</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Snake</button>
+      <button id='acc-6' class="accordion" onClick={() => { accClick('acc-6') }}>Snake</button>
       <div class="panel">
         <p>The classic snake game in your browser</p>
 
@@ -100,7 +100,7 @@ function HomePage() {
         <a className='proj-link' href='https://snake.dickey.gg' target="_blank" rel="noreferrer">Play</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Pong</button>
+      <button id='acc-7' class="accordion" onClick={() => { accClick('acc-7') }}>Pong</button>
       <div class="panel">
         <p>The classic Pong game, in your browser!</p>
 
@@ -109,7 +109,7 @@ function HomePage() {
         <a className='proj-link' href='https://pong.dickey.gg' target="_blank" rel="noreferrer">Play</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Typing Game</button>
+      <button id='acc-8' class="accordion" onClick={() => { accClick('acc-8') }}>Typing Game</button>
       <div class="panel">
         <p>A fun game to test your typing skills</p>
 
@@ -118,7 +118,7 @@ function HomePage() {
         <a className='proj-link' href='https://typing.dickey.gg' target="_blank" rel="noreferrer">Play</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Collatz Calculator</button>
+      <button id='acc-9' class="accordion" onClick={() => { accClick('acc-9') }}>Collatz Calculator</button>
       <div class="panel">
         <p>An algorithm to test numbers against the rules of the Collatz Conjecture</p>
 
@@ -127,7 +127,7 @@ function HomePage() {
         <a className='proj-link' href='https://github.com/dickeyy/Collatz-Calculator' target="_blank" rel="noreferrer">Learn More</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Spacebar Tester</button>
+      <button id='acc-10' class="accordion" onClick={() => { accClick('acc-10') }}>Spacebar Tester</button>
       <div class="panel">
         <p>A game to spam your spacebar as much as you can.</p>
 
@@ -136,7 +136,7 @@ function HomePage() {
         <a className='proj-link' href='https://spacebar.dickey.gg' target="_blank" rel="noreferrer">Play</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Mountain SVG</button>
+      <button id='acc-11' class="accordion" onClick={() => { accClick('acc-11') }}>Mountain SVG</button>
       <div class="panel">
         <p>A cool SVG art of some mountains and ducks</p>
 
@@ -145,7 +145,7 @@ function HomePage() {
         <a className='proj-link' href='https://mountains.dickey.gg' target="_blank" rel="noreferrer">View</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>DVD Animation</button>
+      <button id='acc-12' class="accordion" onClick={() => { accClick('acc-12') }}>DVD Animation</button>
       <div class="panel">
         <p>Watch the DVD logo bounce and try to hit the corners, in your browser</p>
 
@@ -154,7 +154,7 @@ function HomePage() {
         <a className='proj-link' href='https://dvd.dickey.gg' target="_blank" rel="noreferrer">View</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Dickey API</button>
+      <button id='acc-13' class="accordion" onClick={() => { accClick('acc-13') }}>Dickey API</button>
       <div class="panel">
         <p>Dickey API is a free open source web API that does a variety of things</p>
 
@@ -163,7 +163,7 @@ function HomePage() {
         <a className='proj-link' href='https://api.dickey.gg' target="_blank" rel="noreferrer">View Docs</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Wordle Clone</button>
+      <button id='acc-14' class="accordion" onClick={() => { accClick('acc-14') }}>Wordle Clone</button>
       <div class="panel">
         <p>A from scratch clone of the popular game Wordle</p>
 
@@ -172,7 +172,7 @@ function HomePage() {
         <a className='proj-link' href='https://wordle.dickey.gg' target="_blank" rel="noreferrer">Play</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Seeds</button>
+      <button id='acc-15' class="accordion" onClick={() => { accClick('acc-15') }}>Seeds</button>
       <div class="panel">
         <p>The easiest to use bot on Discord</p>
 
@@ -181,7 +181,7 @@ function HomePage() {
         <a className='proj-link' href='https://seedsbot.xyz' target="_blank" rel="noreferrer">Learn More</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Jams</button>
+      <button id='acc-16' class="accordion" onClick={() => { accClick('acc-16') }}>Jams</button>
       <div class="panel">
         <p>The best music bot on Discord</p>
 
@@ -190,7 +190,7 @@ function HomePage() {
         <a className='proj-link' href='https://jamsbot.com' target="_blank" rel="noreferrer">Learn More</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Spotinder</button>
+      <button id='acc-17' class="accordion" onClick={() => { accClick('acc-17') }}>Spotinder</button>
       <div class="panel">
         <p>Tinder... But for Spotify songs.</p>
 
@@ -199,7 +199,7 @@ function HomePage() {
         <a className='proj-link' href='https://spotinder.dickey.gg' target="_blank" rel="noreferrer">Find New Songs</a>
       </div>
 
-      <button class="accordion" onClick={() => { accClick() }}>Spotify HL</button>
+      <button id='acc-18' class="accordion" onClick={() => { accClick('acc-18') }}>Spotify HL</button>
       <div class="panel">
         <p>The higher lower game but for Spotify stats</p>
 
@@ -220,7 +220,20 @@ function HomePage() {
       <br></br>
 
       <div className='footer'>
-        <p>&copy; 2022 Kyle Dickey</p>
+        <span className='footer-p'>&copy; 2022 Kyle Dickey</span>
+
+        <div className='footer-links'>
+          <a href='mailto: kyle@dickey.gg' className="fa fa-solid fa-envelope fa-2x socialIcon-foot"> </a>
+
+          <a href='https://twitter.com/kyledickeyy' target="_blank" rel="noreferrer" className="fa fa-brands fa-twitter fa-2x socialIcon-foot"> </a>
+
+          <a href='https://instagram.com/kyle.dickeyy' target="_blank" rel="noreferrer" className="fa fa-brands fa-instagram fa-2x socialIcon-foot"> </a>
+
+          <a href='https://github.com/dickeyy' target="_blank" rel="noreferrer" className="fa fa-brands fa-github fa-2x socialIcon-foot"> </a>
+
+          <a href='https://twitch.tv/imdickey' target="_blank" rel="noreferrer" className="fa fa-brands fa-twitch fa-2x socialIcon-foot"> </a>
+        </div>
+
       </div>
 
       <br></br>
@@ -235,19 +248,37 @@ function HomePage() {
 export default HomePage;
 
 
-const accClick = () => {
-  var acc = document.getElementsByClassName("accordion");
-  var i;
+function accClick(id) {
+  // var acc = document.getElementsByClassName("accordion");
+  // var i;
 
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.display === "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
-    });
+  // for (i = 0; i < acc.length; i++) {
+  //   acc[i].classList.toggle('active')
+  //   var panel = acc[1].nextElementSibling;
+  //   if (panel.style.display === "block") {
+  //     panel.style.display = "none";
+  //   } else {
+  //     panel.style.display = "block";
+  //   }
+
+  var acc = document.getElementById(id);
+
+  acc.classList.toggle('active');
+  var panel = acc.nextElementSibling;
+  if (panel.style.display === "block") {
+    panel.style.display = "none";
+  } else {
+    panel.style.display = "block";
   }
+
+    // acc[i].addEventListener("click", function() {
+    //   this.classList.toggle("active");
+    //   var panel = this.nextElementSibling;
+    //   if (panel.style.display === "block") {
+    //     panel.style.display = "none";
+    //   } else {
+    //     panel.style.display = "block";
+    //   }
+    // });
+  // }
 }
