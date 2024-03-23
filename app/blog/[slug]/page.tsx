3b4d10b89fb6493e0060369a60a21144dce663b7
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-03-23 00:18:55 -0600

    small blog fix

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -18,7 +18,6 @@ export async function generateMetadata(
     parent: ResolvingMetadata
 ): Promise<Metadata> {
     const post = await getPostData(params.slug);
-    console.log(post.ogImage);
     return {
         title: post.title + " | Kyle Dickey",
         authors: [
@@ -48,6 +47,8 @@ export default async function Page({ params }: { params: { slug: string } }) {
         notFound();
     }
 
+    console.log(post);
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-24 sm:pb-4">
             <div className="md:2/3 mt-8 flex w-full flex-col items-start justify-start gap-4 sm:mt-0 lg:w-1/2">
@@ -55,7 +56,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
                     <BlogViewIncrement id={params.slug} />
                     <Link
                         href="/blog"
-                        className="flex flex-row items-center gap-2 text-foreground transition-all duration-200 ease-in-out"
+                        className="text-foreground flex flex-row items-center gap-2 transition-all duration-200 ease-in-out"
                     >
                         <ArrowLeftIcon className="h-6 w-6" />
                     </Link>
@@ -63,14 +64,14 @@ export default async function Page({ params }: { params: { slug: string } }) {
                 </div>
                 <article className="mb-8 w-full flex-1">
                     <h1 className="text-4xl font-extrabold">{post.title}</h1>
-                    <p className="mt-2 italic text-foreground/60">
+                    <p className="text-foreground/60 mt-2 italic">
                         <Link href="/" className="hover:underline">
                             Kyle Dickey
                         </Link>{" "}
                         · {new Date(post.date * 1000).toLocaleDateString()} · {post.views} view
                         {post.views > 1 && "s"}
                     </p>
-                    <p className="italic text-foreground/60"></p>
+                    <p className="text-foreground/60 italic"></p>
                     <Separator className="my-4" />
                     <div
                         className="blog-content"
