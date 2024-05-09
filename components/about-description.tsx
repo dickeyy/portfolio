75830cfbe0d5b768a54b6777e735b31b6d7847ba
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-05-09 15:24:38 -0600

    update about

--- components/about-description.tsx
+++ components/about-description.tsx
@@ -14,10 +14,13 @@ export default function AboutDescription({ className }: { className?: string }) 
         <div className={cn(className, "flex flex-col items-start text-foreground")}>
             <h2 className="text-2xl font-bold">About</h2>
             <p className="text-md font text-foreground/70">
-                Born and raised near Denver, Colorado. I am a second-year Computer Science major at
-                Colorado State University. Aside from coding, I also enjoy movies, music, SCUBA
-                diving, and playing chess. I am a full-stack dev, however, I have a special place in
-                my heart for frontend work. If you would like to know more, click the buttons below!
+                Born and raised near Denver, Colorado. I am a second-year computer science with a
+                focus in software engineering student at Colorado State University. I have a strong
+                foundation in programming languages such as Javascript / Typescript, Python, Go, and
+                Java. I specialize in creating sophisticated web and mobile applications, embedded
+                systems, complex APIs, and performant infrastructures suitable for any size of
+                project. Aside from coding, I also enjoy movies, music, SCUBA diving, and playing
+                chess. If you would like to know more, or contact me, click the buttons below!
             </p>
             <div className="mt-4 flex w-full flex-col items-start gap-2 sm:flex-row">
                 <Button asChild variant={"default"} size={"lg"} className="w-full">
