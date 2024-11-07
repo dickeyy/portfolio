eed922c5fd23a4556bff8846e5dc3d9acecb429f
Kyle Dickey <kyledickey02@gmail.com>
2024-11-07 21:28:51 +0000

    globalize social urls and handles

---
+++ config.ts
@@ -0,0 +1,27 @@
+export default {
+    contact: {
+        email: "k@kyle.so"
+    },
+    socials: {
+        twitter: {
+            handle: "kyledickeyy",
+            url: "https://twitter.com/kyledickeyy"
+        },
+        github: {
+            handle: "dickeyy",
+            url: "https://github.com/dickeyy"
+        },
+        linkedin: {
+            handle: "kyletdickey",
+            url: "https://www.linkedin.com/in/kyletdickey/"
+        },
+        instagram: {
+            handle: "kyle.dickeyy",
+            url: "https://instagram.com/kyle.dickeyy"
+        },
+        bsky: {
+            handle: "kyle.so",
+            url: "https://bsky.app/profile/kyle.so"
+        }
+    }
+};
