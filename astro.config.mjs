7cff0ff4301e49ef82d844aa7cc329a4ec311d8f
Kyle Dickey <kyledickey02@gmail.com>
2024-12-01 21:47:30 +0000

    remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
-    adapter: vercel(),
+    adapter: node({
+        mode: "standalone"
+    }),
     integrations: [
         tailwind({
             applyBaseStyles: true
